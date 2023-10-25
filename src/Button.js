b50d840ccba66dbee0f90c38f29f6a2c9d7e5845
kty <xodud9625@hanmail.net>
2023-10-25 20:34:50 +0900

    commit

---
+++ src/Button.js
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types";
+import btnStyle from "./Button.module.css";
+
+
+function Button({text}) {
+    return <button className={btnStyle.btn}>{text}</button>
+};
+
+Button.prototype = {
+    text : PropTypes.string.isRequired,
+};
+
+export default Button;
