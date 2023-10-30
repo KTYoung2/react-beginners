c2a89f3cac7e9f73da910f365cfe9f17e741c5b8
kty <xodud9625@hanmail.net>
2023-10-30 22:35:59 +0900

    commit

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+
 function Detail () {
-    return <h1>Detail</h1>
+    const { id } = useParams();
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    useEffect(()=> {
+        const getDtail = async () => {
+            const json = await (
+                await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+            ).json();
+            console.log(json);
+            setMovies(json.data.movie);
+            setLoading(false);
+        };
+        getDtail();
+    }, [id]);
+    return (
+        <div>
+            {loading ? ( 
+             <h1>Dtail Loading ... ⌛</h1> 
+          ) : ( 
+            <div>
+                <img src={movies.large_cover_image} />
+                <h1>Title : {movies.title}</h1>
+                <p>{movies.description_intro}</p>
+                <ul>
+                {movies.genres.map((g) => (
+                    <li key={g}> genres : {g}</li>
+                ))}
+                    <li>runtime: {movies.runtime} minute</li>
+                    <li>Rating : {movies.rating}</li>
+                    <li>Like : {movies.like_count}</li>
+
+                </ul>
+            </div>
+         )}
+        </div>
+    )
 };
 
 export default Detail;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,13 +7,13 @@ function Home () {
     const getMovies = async() => {
       const json = await (
         await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8&sort_by=year"
+          "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
         )
       ).json();
       setMovies(json.data.movies);
       setLoading(false);
     };
-  
+    
     useEffect(() => {
       getMovies()
     }, []);
@@ -27,8 +27,8 @@ function Home () {
             {movies.map((movie) => (
               <Movie
                 key={movie.id} 
+                id={movie.id}
                 coverImg={movie.medium_cover_image}
-                
                 title={movie.title}
                 summary={movie.summary}
                 genres={movie.genres}
