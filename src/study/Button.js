2f2fcdd5811d79f329f8e0954056dc8caebf221c
kty <xodud9625@hanmail.net>
2023-11-09 16:16:03 +0900

    commit

--- src/study/Button.js
+++ src/study/Button.js
@@ -1,13 +1,17 @@
-import PropTypes from "prop-types";
-import btnStyle from "./Button.module.css";
+
+import style from "./Button.module.css";
+/* 리액트는 css도 분활해서 재사용. css 코드를 js객체로 변환시켜줌
+    따라서 해당 css 코드에 작정된 className(여기서는 btn)을 
+    프로퍼티로 접근. 연산자 (.)을 사용해서 이용 가능
+    {style.btn}
+    
+*/
+
+function Button ({text}){
+    return <button className={style.btn}>{text}</button>;
+}
 
 
-function Button({text}) {
-    return <button className={btnStyle.btn}>{text}</button>
-};
 
-Button.prototype = {
-    text : PropTypes.string.isRequired,
-};
 
 export default Button;
